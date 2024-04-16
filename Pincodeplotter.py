6376fb86bb5bc0a57327f96df1893e6a12bba4c5
Stormrag3 <47652327+Stormrag3@users.noreply.github.com>
2024-04-16 19:22:50 +0530

    Update Pincodeplotter.py

--- Pincodeplotter.py
+++ Pincodeplotter.py
@@ -9,6 +9,26 @@ import pandas as pd
 from math import radians, cos, sin, asin, sqrt
 from io import StringIO
 
+def distance(lat1, lat2, lon1, lon2):
+    # The math module contains a function named
+    # radians which converts from degrees to radians.
+    lon1 = radians(lon1)
+    lon2 = radians(lon2)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+
+    c = 2 * asin(sqrt(a))
+
+    # Radius of earth in kilometers. Use 3956 for miles
+    r = 6371
+
+    # calculate the result
+    return (c * r)
 
 def mapmaker(output_pincodes):
 
@@ -94,26 +114,7 @@ def mapmaker(output_pincodes):
     folium_static(m)
     #m.save('map.html')
 
-def distance(lat1, lat2, lon1, lon2):
-    # The math module contains a function named
-    # radians which converts from degrees to radians.
-    lon1 = radians(lon1)
-    lon2 = radians(lon2)
-    lat1 = radians(lat1)
-    lat2 = radians(lat2)
 
-    # Haversine formula
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-
-    c = 2 * asin(sqrt(a))
-
-    # Radius of earth in kilometers. Use 3956 for miles
-    r = 6371
-
-    # calculate the result
-    return (c * r)
 
 #pc1='400097'
 #pc2='400083'
